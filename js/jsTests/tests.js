ddda5920b40dd424af50cb7493a3384393f939b8
MichaelDimi <dimitrm@purdue.edu>
2022-10-01 17:03:43 -0400

    Fixed index.html

--- js/jsTests/tests.js
+++
@@ -1,9 +0,0 @@
-function testUpdateCellType() {
-    organism[0].setType(CellTypes.bud);
-    console.log("NEW: ", organism[0]);
-}
-
-function testChangeDirection() {
-    organism[0].setDirection(Direction.below);
-    console.log("NEW: ", organism[0]);
-}
--- js/tests.js
+++
@@ -1,5 +0,0 @@
-function testSetType() {
-    organism[0].setType(CellTypes.bud);
-    console.log("NEW: ", organism[0]);
-}
-
